c5fa0994b1cebd7d3af77cc2936a4aeb2858be4c
Devyon Miller <devyonmiller1992@yahoo.com>
2022-02-14 19:57:43 -0500

    commit tree 54f504c6e3c75121476381cf9fd39ea66ac906bb

---
+++ test.js
@@ -0,0 +1,40 @@
+
+
+let x = 1
+let y = 2
+let z = x + y
+
+let a = 4
+let b = 6
+let c = a + b 
+
+
+let arr = []
+
+// console.log(arr[3])
+
+// for(let i =0; i < 200; i++){
+//     arr.push(i)
+// }
+
+// console.log(arr)
+
+// console.log('5' === 5)
+// console.log( 5 !== 5)
+
+// console.log(c)
+
+let test = false
+
+
+let firstName = "Devyon"
+let lastName = "Miller"
+
+console.log("my name is "+ firstName + " " + lastName);
+
+
+x = 7
+
+
+
+// console.log("my name is " +  name)
---
+++ testing.js
@@ -0,0 +1,26 @@
+/*
+data types 
+
+string
+number
+boolean
+undefined
+
+arrays //baskets
+objects //baskets
+*/
+
+// Rule 1 ===> For every value we have a key, which we can use to access the value
+// Rule 2 ===> The default value for everything is undefined
+
+
+/*
+Array, String, Boolean, Objects, Numbers, 
+*/
+
+
+const birds = ["blueBird", "greenBird", "redBird"]
+
+const birdsObject = {bird1:"blueBird", bird2:"greenBird", bird3:"redBird"}
+
+console.log(birdsObject["bird1"])
